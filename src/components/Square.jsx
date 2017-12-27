cd4ecad79b91260228c850b521c6d45740180e88
cemilcelik <cemilcelik84@gmail.com>
2017-12-27 09:47:32 +0300

    Make interactive the buttons. Square component convert to the functional.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.props = props;
-    }
-
-    render() {
-        return (
-            <button className="square">
-                { this.props.value }
-            </button>
-        );
-    };
+function Square(props) {
+    return (
+        <button className="square" onClick={ props.onClick }>
+            { props.value }
+        </button>
+    );
 }
 
 export default Square;
